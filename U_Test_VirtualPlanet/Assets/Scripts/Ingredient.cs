9609bc17791dab6a1dfb3a35b155b9ebc6377df5
Fabrício Duarte <fabricioohod@outlook.com>
2023-06-16 01:35:57 -0300

    Add DoTween

--- U_Test_VirtualPlanet/Assets/Scripts/Ingredient.cs
+++ U_Test_VirtualPlanet/Assets/Scripts/Ingredient.cs
@@ -1,8 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Ingredient : MonoBehaviour
 {
     [field: SerializeField] public int ID { get; private set; }
+
+    IEnumerator Start()
+    {
+        transform.localScale = Vector3.zero;
+
+        yield return null;
+
+        transform.DOScale(Vector3.one, .2f).SetEase(Ease.InBounce);
+    }
 }
