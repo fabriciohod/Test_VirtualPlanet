52fb76e151c89baf345d8a68166bdf7fc55db4fa
Fabrício Duarte <fabricioohod@outlook.com>
2023-06-16 19:25:43 -0300

    Audios and background props

--- U_Test_VirtualPlanet/Assets/Scripts/UI/InGameBtn.cs
+++ U_Test_VirtualPlanet/Assets/Scripts/UI/InGameBtn.cs
@@ -6,7 +6,9 @@ using UnityEngine.Events;
 public class InGameBtn : MonoBehaviour, IPointerClickHandler
 {
     public UnityEvent OnClicked;
+    [SerializeField] Timer cooldown;
     Transform btn;
+    bool canClickAgain = true;
 
     void Awake()
     {
@@ -17,6 +19,8 @@ public class InGameBtn : MonoBehaviour, IPointerClickHandler
     {
         btn.DOLocalMoveY(.3f, .1f).SetEase(Ease.InCubic);
         OnClicked?.Invoke();
+        canClickAgain = false;
+        cooldown.StartTimer(() => canClickAgain = true);
         btn.DOLocalMoveY(.5f, .1f).SetEase(Ease.OutCubic).SetDelay(.35f);
     }
 }
